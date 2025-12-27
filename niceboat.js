340b26855ea5a7a19cce75436c12f19a5be672fd
Mogami1121 <lowendd1121@gmail.com>
2025-12-27 22:38:03 +0900

    終了

--- niceboat.js
+++ niceboat.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
-//ここにかいて
+//ここからは舟券発売
 let slist = [
   { number:"1号艇", name:"田中太郎", class:"A1", sex:"男", age:"23", cnt:"東京/東京支部", wht:"51.0"},
   { number:"2号艇", name:"田中太郎", class:"A1", sex:"男", age:"23", cnt:"東京/東京支部", wht:"51.0" },
@@ -27,16 +27,71 @@ app.get("/index", (req, res) => {
   res.render("index");
 });
 
-app.get("/racer", (req, res) => {
-  res.render("racer", { data: slist });
+app.get("/h_racer", (req, res) => {
+  res.render("h_racer", { data: slist });
 });
 
-app.get("/sol", (req, res) => {
-  res.render("sol");
+app.get("/h_sol", (req, res) => {
+  res.render("h_sol");
 });
 
-app.get("/start", (req, res) => {
-  res.render("start",{data: staten});
+app.get("/h_start", (req, res) => {
+  res.render("h_start",{data: staten});
+});
+//ここまで舟券発売
+
+//ここからかくてい　
+app.get("/index2", (req, res) => {
+  res.render("index2");
 });
 
+function shuffle(array) {
+  return array
+    .map(v => ({ v, r: Math.random() }))
+    .sort((a, b) => a.r - b.r)
+    .map(({ v }) => v);
+}
+app.get("/r_kekka", (req, res) => {
+  const rdm = shuffle(rlist).slice(0, 3);
+  res.render("r_kekka", { data: rdm });
+});
+let rlist = [
+  { number:"1号艇", name:"田中太郎"},
+  { number:"2号艇", name:"田中太郎"},
+  { number:"3号艇", name:"佐藤花音"},
+  { number:"4号艇", name:"鈴木悠真"},
+  { number:"5号艇", name:"森下ひより"},
+  { number:"6号艇", name:"高橋健介"},
+];
+
+// app.get("/r_oz", (req, res) => {
+//   res.render("r_oz", { data: rdm });
+  
+//   }
+
+//ここから払戻システム
+app.get("/index3", (req, res) => {
+  res.render("index3", {data:slist});
+});
+
+app.get("/h_toroku", (req, res) => {
+  res.render("h_toroku");
+});
+
+app.get("/h_comp", (req, res) => {
+  res.render("h_comp");
+});
+
+app.post("/add", (req, res) => {
+  slist.push({
+    number: req.body.number,
+    name: req.body.name,
+    class: req.body.class,
+    sex: req.body.sex,
+    age: req.body.age,
+    cnt: req.body.cnt,
+    wht: req.body.wht
+  });
+  res.redirect("/h_comp");
+});
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
